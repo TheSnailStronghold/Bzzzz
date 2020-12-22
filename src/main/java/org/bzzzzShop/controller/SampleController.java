f9b150bc4611fba0a14d82db8e6101964318107a
Maria Nikolaeva <mashanikolaeva99@ro.ru>
2020-12-22 19:50:00 +0300

    Добавлен класс ServiceWorker, в нём добавлен списочек товаров (улья х2, перчатки х3, маска х1, костюм х2). Изменено название класса GlovesOfTheBeekeeper (было Glaves). Класс Basket перемещён из папки orderState в папку models. В классе SampleController добавлена функция для главной страницы магазина (mainPage).

--- src/main/java/org/bzzzzShop/controller/SampleController.java
+++ src/main/java/org/bzzzzShop/controller/SampleController.java
@@ -1,8 +1,8 @@
 package org.bzzzzShop.controller;
 
+import org.bzzzzShop.ServiceWorker;
 import org.bzzzzShop.dto.BasketDTO;
-import org.bzzzzShop.models.Customer;
-import org.bzzzzShop.models.orderState.Basket;
+import org.bzzzzShop.models.Basket;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -10,16 +10,30 @@ import java.util.UUID;
 @RestController
 public class SampleController {
 
+    private ServiceWorker serviceWorker = new ServiceWorker();
+
     //Мы используем методы помеченные GetMapping когда хотим получить информацию
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return "Hello, " + name + "!";
     }
 
+    @GetMapping("/main")
+    public String mainPage() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Welcome, customer! You are now in 'Bzzzz' — the best beekeeping shop!");
+        builder.append("\n");
+        /*  В браузере перенос строки он почему-то не отображает, но в Postman видит.
+            Сдавать нам именно в Postman, так что не страшно, думаю.
+         */
+        builder.append("You are now at beta page, shop is not finished yet. Return soon!");
+        return builder.toString();
+    }
+
     @PostMapping("/findBuyer")
-    public Customer getBuyerById(@RequestParam(value = "id") UUID id) {
+    public String getBuyerById(@RequestParam(value = "id") UUID id) {
         //TODO здесь мы должныы опиать логику поиска покупателя по его уникального идентифкатору в БД
-        return new Customer();
+        return "It`s getBuyerById" + id.toString();
     }
 
     //Мы используем методы помеченные PostMapping когда хотим добавить или сохранить информацию
