cffc2f8d33facf59dc581d6b336d7dda067a55fd
TheSnailStronghold <c.bookworm@yandex.ru>
2020-12-23 17:03:01 +0300

    add class Account and changes in Customer

--- src/main/java/org/bzzzzShop/models/Customer.java
+++ src/main/java/org/bzzzzShop/models/Customer.java
@@ -2,19 +2,19 @@ package org.bzzzzShop.models;
 
 import java.util.Objects;
 
+
 /*Покупатель*/
 public class Customer {
     private String username;
     private String phoneNumber;
     private String email;
-    private String password;
-    //private List<Order> orders;
+    private Account customerAcc;
 
-    public Customer(String username, String phoneNumber, String email, String password) {
-        this.username = username;
+    public Customer(String login, String password, String username, String phoneNumber, String email) {
+        customerAcc = new Account(login, password);
+        this.username=username;
         this.phoneNumber = phoneNumber;
         this.email = email;
-        this.password = password;
     }
 
     @Override
@@ -22,21 +22,22 @@ public class Customer {
         if (this == o) return true;
         if (!(o instanceof Customer)) return false;
         Customer customer = (Customer) o;
-        return username.equals(customer.username);
+        return customerAcc.equals(customer.customerAcc);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(username);
+        return Objects.hash(customerAcc);
     }
 
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
-        str.append(String.format("Имя покупателя: %s\n" +
+        str.append(customerAcc.toString());         //вывод информации об учетной записи
+        str.append(String.format("Имя пользователя: %s\n" +
                         "Номер телфона: %s\n" +
                         "E-mail: %s\n",
-                username, phoneNumber, email));
+                 username, phoneNumber, email));
         str.append("--------------------------------------------\n");
         str.append("История заказов(заказ, дата):\n");
         /*SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY");
@@ -61,14 +62,9 @@ public class Customer {
         return email;
     }
 
-    public String getPassword() {
-        return password;
+    public void setUsername(String username){
+        this.username=username;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
@@ -77,7 +73,4 @@ public class Customer {
         this.email = email;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
